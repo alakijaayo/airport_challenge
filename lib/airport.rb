833db25abc6c003c7545050e34cc23bd7eb97073
alakijayo <alakijaayo@gmail.com>
2018-09-10 13:06:14 +0100

    Take off and landing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,19 @@
 class AirportTerminal
 
-  def land
-    Plane.new
+  attr_reader :plane
+
+  def initialize
+    @planes = []
   end
+
+  def land(plane)
+    @planes << plane
+  end
+
+  def take_off(plane)
+    @planes.delete(plane)
+    p "Plane has left the terminal!"
+  end
+
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,18 @@
 require './lib/airport'
+require './lib/plane'
 
 describe AirportTerminal do
 
   it { is_expected.to respond_to :land }
+
+  it 'allows a plane to land' do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq [plane]
+  end
+
+  it 'informs us a plane has taken off' do
+    plane = Plane.new
+    expect(subject.take_off(plane)).to eq "Plane has left the terminal!"
+  end
 
 end
