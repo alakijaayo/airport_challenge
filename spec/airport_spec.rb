e9aee6e0db4389121fb89c9352982ea3cc2018bd
alakijayo <alakijaayo@gmail.com>
2018-09-12 09:55:32 +0100

    Confirming airport terminal is empty

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,7 @@ require './lib/airport'
 describe AirportTerminal do
 
   it { is_expected.to respond_to :land }
-  
+
   it 'allows a plane to land' do
     plane = Plane.new
     expect(subject.land(plane)).to eq [plane]
@@ -12,6 +12,14 @@ describe AirportTerminal do
   it 'informs us a plane has taken off' do
     plane = Plane.new
     expect(subject.take_off(plane)).to eq "Plane has left the terminal!"
+  end
+
+  # 1 if i take off a plane, it should no longer be in the airport terminal planes array
+
+  it 'lets us know the plane is no longer at the terminal' do
+    plane = Plane.new
+    subject.take_off(plane)
+    expect(subject.planes).not_to eq [plane]
   end
 
 end
